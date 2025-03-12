d4dc880243cb4042f8e2367174da843fb426a30f
Cursyy <kurilets.o.v@gmail.com>
2025-03-12 14:09:15 +0000

    feat: settings update for bilanguage

--- GoRide/settings.py
+++ GoRide/settings.py
@@ -119,13 +119,24 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGES = (
+   ('en', ('English')),
+    ('ga', ('Irish')),
+)
 
-TIME_ZONE = "UTC"
+LANGUAGE_CODE = 'en-us'
+
+MODELTRANSLATION_DEFAULT_LANGUAGE = 'en'
+
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale'),
+]
 
 
 # Static files (CSS, JavaScript, Images)
