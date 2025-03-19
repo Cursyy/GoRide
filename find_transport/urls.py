06acb834d48868a3ba63dc05fbe25e2a0c227b1d
Oleksandr Podynihlazov <x00215403@mytudublin.ie>
2025-03-19 12:50:10 +0000

    api: replace client api call with server call. Move api key to .env

--- find_transport/urls.py
+++ find_transport/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import find_transport, get_vehicles, get_station
+from .views import find_transport, get_vehicles, get_station, get_direction
 
 app_name = "find_transport"
 
@@ -7,4 +7,9 @@ urlpatterns = [
     path("", find_transport, name="find_transport"),
     path("api/vehicles", get_vehicles, name="get_vehicles"),
     path("api/stations", get_station, name="get_station"),
+    path(
+        "api/get_direction/<int:station_id>/<str:lon>/<str:lat>/",
+        get_direction,
+        name="get_direction",
+    ),
 ]
