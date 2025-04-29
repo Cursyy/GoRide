334a3eee32998eb0415c409f7eed07de3e92c8cb
Cursyy <kurilets.o.v@gmail.com>
2025-04-29 14:44:55 +0100

    feat(locale): language update, js fix

--- django/GoRide/urls.py
+++ django/GoRide/urls.py
@@ -3,11 +3,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
+from django.views.i18n import JavaScriptCatalog
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("i18n/", include("django.conf.urls.i18n")),
+    path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
     path("silk/", include("silk.urls", namespace="silk")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
