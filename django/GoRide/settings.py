c38507fb11cfcd22801708cbe53de5e8fcf498cf
Cursyy <kurilets.o.v@gmail.com>
2025-04-29 18:56:47 +0100

    feat(GoRide): add allowed host to settings for localtunnels

--- django/GoRide/settings.py
+++ django/GoRide/settings.py
@@ -30,9 +30,12 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "10.0.2.2",
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'goride.loca.lt']
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+    'https://goride.loca.lt',
 ]
 
 
